8e9031540cdf2c1e0a1da10db8cfd281f0f1edca
User <Mail>
2018-01-08 18:05:44 +0900

    ALLOWED_HOSTSにホスト情報を追加

--- mai/settings.py
+++ mai/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from socket import gethostname, gethostbyname
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -25,7 +26,7 @@ SECRET_KEY = '^&4iw2de(^+_h%*_$^6%auuq*bo%c%v825y83(&0rtt&+i-3-z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost","0.0.0.0","210.140.84.255","dorberu.work"]
+ALLOWED_HOSTS = ["localhost","0.0.0.0",gethostname(),gethostbyname(gethostname()),"210.140.84.255","dorberu.work"]
 
 
 # Application definition
